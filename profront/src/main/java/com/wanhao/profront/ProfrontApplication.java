41a74f01de471acc52e83cfe5ff2556d1119cc41
llh <838088516@qq.com>
2018-07-14 18:07:50 +0800

    前台项目

---
+++ profront/src/main/java/com/wanhao/profront/ProfrontApplication.java
@@ -0,0 +1,12 @@
+package com.wanhao.profront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfrontApplication.class, args);
+    }
+}
