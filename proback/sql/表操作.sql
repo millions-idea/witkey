238b16072b905e61c4484809587724ad1e89fb69
llh <838088516@qq.com>
2018-07-14 17:31:21 +0800

    初次创建后台

---
+++ proback/sql/表操作.sql
@@ -0,0 +1,6 @@
+
+禁用的用户名  中间以 , 隔开
+CREATE TABLE `tb_name_forbidden` (
+  `name` VARCHAR(255) DEFAULT NULL COMMENT '禁用用户名'
+) ENGINE=INNODB DEFAULT CHARSET=utf8
+
---
+++ proback/src/main/java/com/wanhao/proback/ProbackApplication.java
@@ -0,0 +1,15 @@
+package com.wanhao.proback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 后台项目
+ */
+@SpringBootApplication
+public class ProbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProbackApplication.class, args);
+    }
+}
