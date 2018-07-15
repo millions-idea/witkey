ef353d5216705209cb61378ce4c40424be99f6e8
llh <838088516@qq.com>
2018-07-15 12:24:37 +0800

    查找所有

--- proback/src/main/java/com/wanhao/proback/service/admin/AdminService.java
+++ proback/src/main/java/com/wanhao/proback/service/admin/AdminService.java
@@ -2,6 +2,8 @@ package com.wanhao.proback.service.admin;
 
 import com.wanhao.proback.bean.admin.Admin;
 
+import java.util.List;
+
 /**
  * Created by LiuLiHao on 2018/7/15 9:32.
  * 描述：
@@ -11,4 +13,9 @@ public interface AdminService {
 
     Admin findByName(String username);
 
+    /**
+     * 查找所有
+     * @return
+     */
+    List<Admin> findAll();
 }
--- proback/src/main/java/com/wanhao/proback/service/impl/admin/AdminServiceImpl.java
+++ proback/src/main/java/com/wanhao/proback/service/impl/admin/AdminServiceImpl.java
@@ -5,6 +5,9 @@ import com.wanhao.proback.dao.admin.AdminMapper;
 import com.wanhao.proback.service.admin.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * Created by LiuLiHao on 2018/7/15 9:32.
@@ -12,16 +15,24 @@ import org.springframework.stereotype.Service;
  * 作者： LiuLiHao
  */
 @Service
+@Transactional
 public class AdminServiceImpl implements AdminService {
 
     @Autowired
     AdminMapper adminMapper;
 
     @Override
+    @Transactional(readOnly = true)
     public Admin findByName(String username) {
         //根据姓名查找
         Admin admin = new Admin();
         admin.setUsername(username);
         return adminMapper.selectOne(admin);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Admin> findAll() {
+        return adminMapper.selectAll();
     }
 }
